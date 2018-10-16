c307b401814f461ec319f9b496d8a0bf970ae4d5
yzbai <yzbai@DESKTOP-8020AID>
2018-10-16 14:29:13 +0800

    33

---
+++ src/HB.Infrastructure.Aliyun/AliyunOptions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HB.Infrastructure.Aliyun
+{
+    public class AliyunOptions : IOptions<AliyunOptions>
+    {
+        public AliyunOptions Value => this;
+
+        public IList<AliyunProductOptions> Products { get; set; }
+    }
+
+    public class AliyunProductOptions : IOptions<AliyunProductOptions>
+    {
+        public AliyunProductOptions Value => this;
+
+        public string ProductName { get; set; }
+
+        public string RegionId { get; set; }
+
+        public string AccessUserName { get; set; }
+
+        public string AccessKeyId { get; set; }
+
+        public string AccessKeySecret { get; set; }
+
+        public string Endpoint { get; set; }
+    }
+}
---
+++ src/HB.Infrastructure.Aliyun/IAcsClientManager.cs
@@ -0,0 +1,11 @@
+﻿using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Profile;
+
+namespace HB.Infrastructure.Aliyun
+{
+    public interface IAcsClientManager
+    {
+        void AddProfile(string productName, IClientProfile profile);
+        IAcsClient GetAcsClient(string productName);
+    }
+}
