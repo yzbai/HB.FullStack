90e269d3e56c4903325a2bf46747c1360cd894b5
yzbai <yzbai@DESKTOP-8020AID>
2018-10-16 15:24:32 +0800

    add

---
+++ src/HB.Infrastructure.Aliyun/Vod/AliyunVodOptions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HB.Infrastructure.Aliyun.Vod
+{
+    public class AliyunVodOptions : IOptions<AliyunVodOptions>
+    {
+        public AliyunVodOptions Value => this;
+
+        public string ProductName { get; set; }
+    }
+}
