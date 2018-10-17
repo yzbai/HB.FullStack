4858b41c5e49d96ba71a447e9ed4ca230ead6d6a
yzbai <yzbai@DESKTOP-8020AID>
2018-10-17 15:22:58 +0800

    dd

--- src/HB.Component.Resource/Sms/ISmsBiz.cs
+++ src/HB.Component.Resource/Sms/ISmsBiz.cs
@@ -14,7 +14,7 @@ namespace HB.Compnent.Resource.Sms
         /// <param name="mobile"></param>
         /// <param name="code"></param>
         /// <returns></returns>
-        Task<SendResponse> SendIdentityValidationCode(string mobile, out string code);
+        Task<SendResponse> SendValidationCode(string mobile, out string code);
 
     }
 }
