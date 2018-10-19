a785c160d7ae13563386ca0f72034059cf1b95b0
yzbai <yzbai@DESKTOP-8020AID>
2018-10-19 15:38:13 +0800

    xx

--- src/HB.Framework.Common/GlobalSettings.cs
+++ src/HB.Framework.Common/GlobalSettings.cs
@@ -4,31 +4,6 @@ namespace System
 {
     public static class GlobalSettings
     {
-        //private static CultureInfo GlobalSettings.Culture = CultureInfo.InvariantCulture;
-        //private static StringComparison _comparison = StringComparison.InvariantCulture;
-        //private static StringComparison _comparisonIgnoreCase = StringComparison.InvariantCultureIgnoreCase;
-
-        //public static CultureInfo Culture { get => GlobalSettings.Culture; }
-
-        //public static StringComparison Comparison { get => _comparison; }
-
-        //public static StringComparison ComparisonIgnoreCase { get => _comparisonIgnoreCase; }
-
-        //public static void SetCulture(CultureInfo cultureInfo)
-        //{
-        //    GlobalSettings.Culture = cultureInfo;
-        //}
-
-        //public static void SetComparison(StringComparison stringComparison)
-        //{
-        //    _comparison = stringComparison;
-        //}
-
-        //public static void SetComparisonIgnoreCase(StringComparison stringComparison)
-        //{
-        //    _comparisonIgnoreCase = stringComparison;
-        //}
-
         public static CultureInfo Culture { get; set; } = CultureInfo.InvariantCulture;
 
         public static StringComparison Comparison { get; set; } = StringComparison.InvariantCulture;
