0923aadfde71cddf89fa2c1f70de7d9f2b86267d
yzbai <yzbai@brlite.com>
2018-11-13 07:05:53 +0800

    xxx

---
+++ src/HB.Framework.Common/LogHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Extensions.Logging
+{
+    public static class LogHelper
+    {
+        private static ILogger _logger;
+
+        public static void SetGlobalLogger(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public static ILogger GlobalLogger
+        {
+            get
+            {
+                return _logger;
+            }
+        }
+    }
+}
