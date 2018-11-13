d9ddf2cb3cabe059a5522d4a8ddad71a2d7e7728
yzbai <yzbai@brlite.com>
2018-11-14 06:34:27 +0800

    xx

--- src/HB.Framework.Common/Utility/CertificateUtil.cs
+++ src/HB.Framework.Common/Utility/CertificateUtil.cs
@@ -7,9 +7,26 @@ namespace System.Security.Cryptography.X509Certificates
 {
     public class CertificateUtil
     {
+        /// <summary>
+        /// 在CurrentUser中和LocalMachine中寻找
+        /// </summary>
+        /// <param name="subjectName"></param>
+        /// <returns></returns>
+        public static X509Certificate2 GetBySubject(string subjectName)
+        {
+            X509Certificate2 cert = GetBySubject(subjectName, StoreLocation.CurrentUser) ?? GetBySubject(subjectName, StoreLocation.LocalMachine);
+
+            if (cert == null)
+            {
+                LogHelper.GlobalLogger.LogCritical("证书找不到，SubjectName : {0}", subjectName);
+            }
+
+            return cert;
+        }
+
+
         public static X509Certificate2 GetBySubject(string subjectName, StoreLocation storeLocation)
         {
-            LogHelper.GlobalLogger.LogInformation("寻找证书......");
             using (X509Store store = new X509Store(storeLocation))
             {
                 store.Open(OpenFlags.ReadOnly | OpenFlags.OpenExistingOnly);
@@ -18,8 +35,6 @@ namespace System.Security.Cryptography.X509Certificates
 
                 if (collection.Count != 1)
                 {
-                    LogHelper.GlobalLogger.LogCritical("找不到证书 Subject：{0}", subjectName);
-
                     return null;
                 }
 
@@ -27,37 +42,34 @@ namespace System.Security.Cryptography.X509Certificates
             }
         }
 
-        public static X509Certificate2 GetBySubject(string subjectName, StoreLocation storeLocation, StoreName storeName)
+        public static X509Certificate2 GetByThumbprint(string thumbprint)
         {
-            using (X509Store store = new X509Store(storeName, storeLocation))
+            X509Certificate2 cert = GetByThumbprint(thumbprint, StoreLocation.CurrentUser) ?? GetByThumbprint(thumbprint, StoreLocation.LocalMachine);
+
+            if (cert == null)
             {
-                store.Open(OpenFlags.MaxAllowed);
-
-                X509Certificate2Collection collection = store.Certificates.Find(X509FindType.FindBySubjectDistinguishedName, subjectName, true);
-
-                if (collection.Count != 1)
-                {
-                    LogHelper.GlobalLogger.LogCritical("找不到证书 Subject：{0}", subjectName);
-
-                    return null;
-                }
-
-                return collection[0];
+                LogHelper.GlobalLogger.LogCritical("证书找不到, thumbprint : {0}", thumbprint);
             }
+
+            return cert;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="thumbprint"></param>
+        /// <param name="storeLocation"></param>
+        /// <returns></returns>
         public static X509Certificate2 GetByThumbprint(string thumbprint, StoreLocation storeLocation)
         {
             using (X509Store store = new X509Store(storeLocation))
             {
                 store.Open(OpenFlags.ReadOnly | OpenFlags.OpenExistingOnly);
 
-                X509Certificate2Collection collection = store.Certificates.Find(X509FindType.FindByThumbprint, thumbprint, false);
+                X509Certificate2Collection collection = store.Certificates.Find(X509FindType.FindByThumbprint, thumbprint, true);
 
                 if (collection.Count != 1)
                 {
-                    LogHelper.GlobalLogger.LogCritical("找不到证书 Thumbprint：{0}", thumbprint);
-
                     return null;
                 }
 
