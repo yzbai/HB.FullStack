f886b5da255ebff00929eac3c3fe398b4d9a750e
yzbai <yzbai@DESKTOP-8020AID>
2018-11-27 15:27:53 +0800

    auto_commit

---
+++ src/HB.Infrastructure.MongoDB/MongoDBServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace HB.Infrastructure.MongoDB
+{
+    public static class MongoDBServiceCollectionExtensions
+    {
+        public static IServiceCollection AddMongoDB(this IServiceCollection serviceCollection, IConfiguration configuration)
+        {
+            return null;
+        }
+    }
+}
