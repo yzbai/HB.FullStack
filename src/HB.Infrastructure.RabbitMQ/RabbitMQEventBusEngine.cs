71ab3faa9c01bf36b2eb26d2d5093b9525c8cb1b
yzbai <yzbai@brlite.com>
2018-12-11 07:13:31 +0800

    auto_commit

--- src/HB.Infrastructure.RabbitMQ/RabbitMQEventBusEngine.cs
+++ src/HB.Infrastructure.RabbitMQ/RabbitMQEventBusEngine.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Concurrent;
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using HB.Framework.DistributedQueue;
 using HB.Framework.EventBus;
 using HB.Framework.EventBus.Abstractions;
-using HB.Framework.KVStore;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using RabbitMQ.Client;
 
 namespace HB.Infrastructure.RabbitMQ
 {
@@ -15,24 +16,36 @@ namespace HB.Infrastructure.RabbitMQ
         private ILogger _logger;
         private RabbitMQEngineOptions _options;
         private IRabbitMQConnectionManager _connectionManager;
-
-        private IKVStore _kvStore;
-
-
-        public RabbitMQEventBusEngine(IOptions<RabbitMQEngineOptions> options, ILogger<RabbitMQEventBusEngine> logger, IRabbitMQConnectionManager connectionManager, IKVStore kvStore)
+        private IDistributedQueue _queue;
+        private IDictionary<string, PublishWorkerManager> _managers;
+        
+        public RabbitMQEventBusEngine(IOptions<RabbitMQEngineOptions> options, ILogger<RabbitMQEventBusEngine> logger, IRabbitMQConnectionManager connectionManager, IDistributedQueue queue)
         {
             _logger = logger;
             _options = options.Value;
             _connectionManager = connectionManager;
 
-            _kvStore = kvStore;
+            _queue = queue;
 
-            //初始化 自拥有线程池
+            _managers = new Dictionary<string, PublishWorkerManager>();
 
-            //线程池开始工作
+           //初始化每一个PublishWorkerManager
+           //放入——managers
 
-            Thread thread = new Thread(new ThreadStart(() => { }));
-            thread.Start();
+            foreach (RabbitMQConnectionSetting connectionSetting in _options.ConnectionSettings)
+            {
+                _managers.Add(connectionSetting.BrokerName, new PublishWorkerManager(connectionSetting, _connectionManager, _queue));
+            }
+        }
+
+        private void NotifyPublishToRabbitMQ(string brokerName)
+        {
+            //让 broker 名字为brokerName的 publishmanager开始工作
+            //publishmanager开始创建Task, publishmanager
+
+            //这里已经确保brokerName是存在的了,之前再PublishAsync里已经检查过
+
+            _managers[brokerName].NotifyPublishComming();
         }
 
         public async Task<bool> PublishAsync(string brokerName, EventMessage eventMessage)
@@ -41,11 +54,35 @@ namespace HB.Infrastructure.RabbitMQ
             //缓存池内容不能丢，所以用抗击打的Redis来存储
             //注意取消息后需要从kvstore删除
 
-            EventMessageEntity entity = new EventMessageEntity(brokerName, eventMessage);
+            if (!IsBrokerExists(brokerName))
+            {
+                return false;
+            }
 
-            KVStoreResult storeResult = await _kvStore.AddAsync(entity);
+            IDistributedQueueResult queueResult = await _queue.Push(queueName: brokerName, data: eventMessage);
 
-            return storeResult.IsSucceeded();
+            if (queueResult.IsSucceeded())
+            {
+                NotifyPublishToRabbitMQ(brokerName);
+
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool IsBrokerExists(string brokerName)
+        {
+            RabbitMQConnectionSetting connectionSetting = _options.GetConnectionSetting(brokerName);
+
+            if (connectionSetting == null)
+            {
+                _logger.LogCritical($"当前没有broker为{brokerName}的RabbitMQ。");
+
+                return false;
+            }
+
+            return true;
         }
     }
 }
---
+++ src/HB.Infrastructure.Redis/Queue/RedisQueue.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HB.Framework.DistributedQueue;
+using Microsoft.Extensions.Logging;
+
+namespace HB.Infrastructure.Redis.Queue
+{
+    public class RedisQueue : RedisEngineBase, IDistributedQueue
+    {
+        private ILogger _logger;
+
+        public RedisQueue(RedisEngineOptions options, ILogger<RedisQueue> logger) : base(options, logger)
+        {
+            _logger = logger;
+        }
+    }
+}
