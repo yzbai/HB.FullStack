d46ebab23ad9aacf1ed3c71dc86c0f058d3df60d
yzbai <yzbai@DESKTOP-8020AID>
2018-12-10 15:28:49 +0800

    auto_commit

---
+++ src/HB.Framework.Common/Threading/FixedThreadPool.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HB.Framework.Common.Threading
+{
+    public class FixedThreadPoolOptions
+    {
+        public int MinThreadNumber { get; set; } = 10;
+
+        public int MaxThreadNumber { get; set; } = 1000;
+    }
+
+    public class FixedThreadPool
+    {
+        private FixedThreadPoolOptions _options;
+
+        public FixedThreadPool(FixedThreadPoolOptions options)
+        {
+            _options = options;
+        }
+
+         
+    }
+}
