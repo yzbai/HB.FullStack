876a3e8c37d4f9d003e6aadf8094125b0c53658d
yzbai <yzbai@DESKTOP-8020AID>
2018-12-19 13:25:03 +0800

    修改序列化

--- src/HB.Framework.KVStore/Engine/IKVStoreEngine.cs
+++ src/HB.Framework.KVStore/Engine/IKVStoreEngine.cs
@@ -7,20 +7,19 @@ namespace HB.Framework.KVStore.Engine
 {
     public interface IKVStoreEngine : IKVStoreEngineAsync
     {
-        byte[]              EntityGet(string storeName, int storeIndex, string entityName, string entityKey);
-        IEnumerable<byte[]> EntityGet(string storeName, int storeIndex, string entityName, IEnumerable<string> entityKeys);      
-        IEnumerable<byte[]> EntityGetAll(string storeName, int storeIndex, string entityName);
+        string              EntityGet(string storeName, int storeIndex, string entityName, string entityKey);
+        IEnumerable<string> EntityGet(string storeName, int storeIndex, string entityName, IEnumerable<string> entityKeys);      
+        IEnumerable<string> EntityGetAll(string storeName, int storeIndex, string entityName);
 
-        KVStoreResult       EntityAdd(string storeName, int storeIndex, string entityName, string entityKey, byte[] entityValue);
-        KVStoreResult       EntityAdd(string storeName, int storeIndex, string entityName, IEnumerable<string> entityKeys, IEnumerable<byte[]> entityValues);
+        KVStoreResult       EntityAdd(string storeName, int storeIndex, string entityName, string entityKey, string entityJson);
+        KVStoreResult       EntityAdd(string storeName, int storeIndex, string entityName, IEnumerable<string> entityKeys, IEnumerable<string> entityJsons);
 
-        KVStoreResult       EntityUpdate(string storeName, int storeIndex, string entityName, string entityKey, byte[] entityValue, int entityVersion);
-        KVStoreResult       EntityUpdate(string storeName, int storeIndex, string entityName, IEnumerable<string> entityKeys, IEnumerable<byte[]> entityValues, IEnumerable<int> entityVersions);
+        KVStoreResult       EntityUpdate(string storeName, int storeIndex, string entityName, string entityKey, string entityJson, int entityVersion);
+        KVStoreResult       EntityUpdate(string storeName, int storeIndex, string entityName, IEnumerable<string> entityKeys, IEnumerable<string> entityJsons, IEnumerable<int> entityVersions);
 
         KVStoreResult       EntityDelete(string storeName, int storeIndex, string entityName, string entityKey, int entityVersion);
         KVStoreResult       EntityDelete(string storeName, int storeIndex, string entityName, IEnumerable<string> entityKeys, IEnumerable<int> entityVersions);
         KVStoreResult       EntityDeleteAll(string storeName, int storeIndex, string entityName);
         
-
     }
 }
--- src/HB.Framework.KVStore/Engine/IKVStoreEngineAsync.cs
+++ src/HB.Framework.KVStore/Engine/IKVStoreEngineAsync.cs
@@ -7,15 +7,15 @@ namespace HB.Framework.KVStore.Engine
 {
     public interface IKVStoreEngineAsync : IDisposable
     {
-        Task<byte[]>                EntityGetAsync(string storeName, int storeIndex, string entityName, string entityKey);
-        Task<IEnumerable<byte[]>>   EntityGetAsync(string storeName, int storeIndex, string entityName, IEnumerable<string> entityKeys);
-        Task<IEnumerable<byte[]>>   EntityGetAllAsync(string storeName, int storeIndex, string entityName);
+        Task<string>                EntityGetAsync(string storeName, int storeIndex, string entityName, string entityKey);
+        Task<IEnumerable<string>>   EntityGetAsync(string storeName, int storeIndex, string entityName, IEnumerable<string> entityKeys);
+        Task<IEnumerable<string>>   EntityGetAllAsync(string storeName, int storeIndex, string entityName);
 
-        Task<KVStoreResult>         EntityAddAsync(string storeName, int storeIndex, string entityName, string entityKey, byte[] entityValue);
-        Task<KVStoreResult>         EntityAddAsync(string storeName, int storeIndex, string entityName, IEnumerable<string> entityKeys, IEnumerable<byte[]> entityValues);
+        Task<KVStoreResult>         EntityAddAsync(string storeName, int storeIndex, string entityName, string entityKey, string entityJson);
+        Task<KVStoreResult>         EntityAddAsync(string storeName, int storeIndex, string entityName, IEnumerable<string> entityKeys, IEnumerable<string> entityJsons);
 
-        Task<KVStoreResult>         EntityUpdateAsync(string storeName, int storeIndex, string entityName, string entityKey, byte[] entityValue, int entityVersion);
-        Task<KVStoreResult>         EntityUpdateAsync(string storeName, int storeIndex, string entityName, IEnumerable<string> entityKeys, IEnumerable<byte[]> entityValues, IEnumerable<int> entityVersions);
+        Task<KVStoreResult>         EntityUpdateAsync(string storeName, int storeIndex, string entityName, string entityKey, string entityJson, int entityVersion);
+        Task<KVStoreResult>         EntityUpdateAsync(string storeName, int storeIndex, string entityName, IEnumerable<string> entityKeys, IEnumerable<string> entityJsons, IEnumerable<int> entityVersions);
 
         Task<KVStoreResult>         EntityDeleteAsync(string storeName, int storeIndex, string entityName, string entityKey, int entityVersion);
         Task<KVStoreResult>         EntityDeleteAsync(string storeName, int storeIndex, string entityName, IEnumerable<string> entityKeys, IEnumerable<int> entityVersions);
