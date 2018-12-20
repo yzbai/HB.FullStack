4c7b8f99733fa36154108362a90f65df79d7d38e
yzbai <yzbai@brlite.com>
2018-12-21 07:26:39 +0800

    auto_commit

--- src/HB.Infrastructure.RabbitMQ/RabbitMQServiceCollectionExtensions.cs
+++ src/HB.Infrastructure.RabbitMQ/RabbitMQServiceCollectionExtensions.cs
@@ -15,6 +15,7 @@ namespace Microsoft.Extensions.DependencyInjection
 
             services.Configure<RabbitMQEngineOptions>(configuration);
 
+            services.AddSingleton<IRabbitMQConnectionManager, RabbitMQConnectionManager>();
             services.AddSingleton<IEventBusEngine, RabbitMQEventBusEngine>();
 
             return services;
