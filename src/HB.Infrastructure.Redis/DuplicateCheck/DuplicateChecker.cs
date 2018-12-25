75f0c4214fec87aa96252ebed01f72261c07b3ad
yzbai <yzbai@brlite.com>
2018-12-25 08:13:13 +0800

    auto_commit

--- src/HB.Infrastructure.Redis/DuplicateCheck/DuplicateChecker.cs
+++ src/HB.Infrastructure.Redis/DuplicateCheck/DuplicateChecker.cs
@@ -1,10 +1,109 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Text;
+using HB.Framework.Common;
+using StackExchange.Redis;
 
 namespace HB.Infrastructure.Redis.DuplicateCheck
 {
-    public class DuplicateChecker : IDuplicateChecker
+    /// <summary>
+    /// 使用方法：每次都要先Lock，最后Release
+    /// </summary>
+    public class DuplicateChecker
     {
+        private string _instanceName;
+        private long _aliveSeconds;
+        private IRedisInstanceManager _redisInstanceManager;
+
+        private ConcurrentDictionary<string, string> _tokenDict = new ConcurrentDictionary<string, string>();
+
+        public DuplicateChecker(IRedisInstanceManager redisInstanceManager, string instanceName, long aliveSeconds)
+        {
+            _redisInstanceManager = redisInstanceManager;
+            _instanceName = instanceName;
+            _aliveSeconds = aliveSeconds;
+        }
+
+        public bool Lock(string setName, string id, out string token)
+        {
+            token = SecurityHelper.CreateUniqueToken();
+
+            string tokenDictKey = TokenDictKey(setName, id);
+
+            if (_tokenDict.TryAdd(tokenDictKey, token))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Release(string setName, string id, string token)
+        {
+            if (CheckToken(setName, id, token))
+            {
+                _tokenDict.TryRemove(TokenDictKey(setName, id), out string storedValue);
+            }
+        }
+
+        public bool? IsExist(string setName, string id, string token)
+        {
+            if (CheckToken(setName, id, token))
+            {
+                IDatabase database = _redisInstanceManager.GetDatabase(_instanceName);
+
+                if (database.SortedSetRank(setName, id) == null)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+
+            return null;
+        }
+
+        public void Add(string setName, string id, long timestamp, string token)
+        {
+            if (CheckToken(setName, id, token))
+            {
+                IDatabase database = _redisInstanceManager.GetDatabase(_instanceName);
+
+                database.SortedSetAdd(setName, id, timestamp, CommandFlags.None);
+            }
+
+            ClearTimeout(setName);
+        }
+
+        private void ClearTimeout(string setName)
+        {
+            long stopTimestamp = DataConverter.CurrentTimestampSeconds() - _aliveSeconds;
+
+            IDatabase database = _redisInstanceManager.GetDatabase(_instanceName);
+
+            //寻找小于stopTimestamp的，删除他们
+
+            database.SortedSetRemoveRangeByScore(setName, 0, stopTimestamp);
+
+        }
+
+        private static string TokenDictKey(string setName, string id)
+        {
+            return setName + "_" + id;
+        }
+
+        private bool CheckToken(string setName, string id, string token)
+        {
+            if (_tokenDict.TryGetValue(TokenDictKey(setName, id), out string storedToken))
+            {
+                if(storedToken.Equals(token, GlobalSettings.Comparison))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
     }
 }
