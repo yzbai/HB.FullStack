82a9540df0b1c77eca23b6e2a6b43f663991fb93
yzbai <yzbai@DESKTOP-8020AID>
2018-12-29 13:20:28 +0800

    auto_commit

---
+++ src/HB.Infrastructure.Aliyun/Oss/IAliyunOssService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using HB.Infrastructure.Aliyun.Sts;
+
+namespace HB.Infrastructure.Aliyun.Oss
+{
+    public interface IAliyunOssService
+    {
+        Task<StsRoleCredential> GetDirectoryReadRoleCredentialAsync(string bucket, string directory, string roleSessionName);
+        Task<StsRoleCredential> GetDirectoryWriteRoleCredentialAsync(string bucket, string directory, string roleSessionName);
+        Task<StsRoleCredential> GetUserReadRoleCredentialAsync(string bucket, string userGuid);
+        Task<StsRoleCredential> GetUserWriteRoleCredentialAsync(string bucket, string userGuid);
+    }
+}
