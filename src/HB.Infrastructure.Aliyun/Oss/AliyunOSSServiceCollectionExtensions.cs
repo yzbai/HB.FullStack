157a02ef682d0e3f090938bd5663d4ce19efe0d5
yzbai <yzbai@DESKTOP-8020AID>
2018-12-29 13:20:28 +0800

    auto_commit

--- src/HB.Infrastructure.Aliyun/Oss/AliyunOSSServiceCollectionExtensions.cs
+++ src/HB.Infrastructure.Aliyun/Oss/AliyunOSSServiceCollectionExtensions.cs
@@ -8,20 +8,24 @@ namespace Microsoft.Extensions.DependencyInjection
 {
     public static class AliyunOssServiceCollectionExtensions
     {
-        public IServiceCollection AddAliyunOss(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection AddAliyunOss(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddOptions();
 
             services.Configure<AliyunOssOptions>(configuration);
+
+            services.AddSingleton<IAliyunOssService, AliyunOssService>();
+
             return services;
         }
 
-        public IServiceCollection AddAliyunOss(this IServiceCollection services, Action<AliyunOssOptions> action)
+        public static IServiceCollection AddAliyunOss(this IServiceCollection services, Action<AliyunOssOptions> action)
         {
             services.AddOptions();
 
             services.Configure(action);
 
+            services.AddSingleton<IAliyunOssService, AliyunOssService>();
 
             return services;
         }
---
+++ src/HB.Infrastructure.Aliyun/Sts/StsRoleCredential.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HB.Infrastructure.Aliyun.Sts
+{
+    public class StsRoleCredential
+    {
+        public string RequestId { get; set; }
+
+        public string SecurityToken { get; set; }
+
+        public string AccessKeyId { get; set; }
+
+        public string AccessKeySecret { get; set; }
+
+        public string Expiration { get; set; }
+
+        public string AssumedRoleId { get; set; }
+
+        public string AssumedRoleName { get; set; }
+
+    }
+}
