0c8bbb845cdbcbed3aefbaeb64a8cdb29856a470
yzbai <yzbai@brlite.com>
2018-12-29 08:05:12 +0800

    auto_commit

---
+++ src/HB.Infrastructure.Aliyun/Oss/AliyunOSSOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Options;
+
+namespace HB.Infrastructure.Aliyun.Oss
+{
+    public class AliyunOssOptions : IOptions<AliyunOssOptions>
+    {
+        public AliyunOssOptions Value => this;
+
+        public string ProductName { get; set; }
+    }
+}
---
+++ src/HB.Infrastructure.Aliyun/Oss/AliyunOssService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Aliyun.Acs.Sts.Model.V20150401;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace HB.Infrastructure.Aliyun.Oss
+{
+    public class AliyunOssService
+    {
+        public AliyunOssService(IOptions<AliyunOssOptions> options, IAcsClientManager acsClientManager, ILogger<AliyunOssService> logger)
+        {
+
+        }
+
+        public AssumeRoleResponse GetToken
+    }
+}
