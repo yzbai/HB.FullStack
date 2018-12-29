08f954deb3fc7cc13c97af9fcbaa3296c04563e0
yzbai <yzbai@brlite.com>
2018-12-29 08:05:12 +0800

    auto_commit

---
+++ src/HB.Infrastructure.Aliyun/Oss/AliyunOSSServiceCollectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HB.Infrastructure.Aliyun.Oss;
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class AliyunOssServiceCollectionExtensions
+    {
+        public IServiceCollection AddAliyunOss(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddOptions();
+
+            services.Configure<AliyunOssOptions>(configuration);
+            return services;
+        }
+
+        public IServiceCollection AddAliyunOss(this IServiceCollection services, Action<AliyunOssOptions> action)
+        {
+            services.AddOptions();
+
+            services.Configure(action);
+
+
+            return services;
+        }
+    }
+}
