d8e62004f536bc038f0c20be740b05e09b37146f
yzbai <yzbai@DESKTOP-8020AID>
2018-12-29 13:20:28 +0800

    auto_commit

--- src/HB.Infrastructure.Aliyun/Oss/AliyunOSSOptions.cs
+++ src/HB.Infrastructure.Aliyun/Oss/AliyunOSSOptions.cs
@@ -1,14 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Microsoft.Extensions.Options;
 
 namespace HB.Infrastructure.Aliyun.Oss
 {
+    public class BucketSettings
+    {
+        public string BucketName { get; set; }
+
+        public string ReadArn { get; set; }
+
+        public string WriteArn { get; set; }
+
+        public string UserDirectoryPath { get; set; }
+
+        public int StsExpireSeconds { get; set; } = 3600;
+    }
+
     public class AliyunOssOptions : IOptions<AliyunOssOptions>
     {
         public AliyunOssOptions Value => this;
 
         public string ProductName { get; set; }
+
+        public IList<BucketSettings> Buckets { get; set; } = new List<BucketSettings>();
+
+        public BucketSettings GetBucketSettings(string bucketName)
+        {
+            return Buckets.FirstOrDefault(s => s.BucketName.Equals(bucketName, GlobalSettings.Comparison));
+        }
     }
 }
