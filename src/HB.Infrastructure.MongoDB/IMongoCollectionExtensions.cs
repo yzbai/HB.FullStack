0b62a397c81f743428a3d732b0b8452b675a1ae2
yzbai <yzbai@brlite.com>
2019-01-07 16:15:14 +0800

    auto_commit

--- src/HB.Infrastructure.MongoDB/IMongoCollectionExtensions.cs
+++ src/HB.Infrastructure.MongoDB/IMongoCollectionExtensions.cs
@@ -2,6 +2,7 @@
 using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,11 +18,11 @@ namespace HB.Infrastructure.MongoDB
             return cursor.FirstOrDefault();
         }
 
-        public static async Task<IEnumerable<T>> GetByIdsAsync<T>(this IMongoCollection<T> collection, IEnumerable<string> ids) where T : DocumentStoreEntity, new()
+        public static async Task<IList<T>> GetByIdsAsync<T>(this IMongoCollection<T> collection, IEnumerable<string> ids) where T : DocumentStoreEntity, new()
         {
             IAsyncCursor<T> cursor = await collection.FindAsync(Builders<T>.Filter.In(t => t.Id, ids)).ConfigureAwait(false);
 
-            return cursor.ToEnumerable();
+            return cursor.ToEnumerable().ToList();
         }
 
         public static T GetById<T>(this IMongoCollection<T> collection, string id) where T : DocumentStoreEntity, new()
@@ -29,12 +30,11 @@ namespace HB.Infrastructure.MongoDB
             return collection.Find(t => t.Id == id).FirstOrDefault();
         }
 
-        public static IEnumerable<T> GetByIds<T>(this IMongoCollection<T> collection, IEnumerable<string> ids) where T : DocumentStoreEntity, new()
+        public static IList<T> GetByIds<T>(this IMongoCollection<T> collection, IEnumerable<string> ids) where T : DocumentStoreEntity, new()
         {
             IFindFluent<T, T> fluent = collection.Find(Builders<T>.Filter.In(t => t.Id, ids));
 
-            return fluent.ToEnumerable();
+            return fluent.ToEnumerable().ToList();
         }
-
     }
 }
