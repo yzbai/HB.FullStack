4f42c3c82abc16934717b4158b6ee345275ff042
yzbai <yzbai@brlite.com>
2019-01-24 15:22:59 +0800

    auto_commit

--- src/HB.Framework.Common/Utility/DataConverter.cs
+++ src/HB.Framework.Common/Utility/DataConverter.cs
@@ -579,8 +579,93 @@ namespace HB.Framework.Common
 
         #endregion
 
+        #region List to String
+
+        private static char[] CommonSeparator = new char[] { ',', '-', '_', '/', '.', '$', '#', '@' };
+
+        public static string ListToString(IEnumerable<string> list)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            foreach (string str in list)
+            {
+                builder.Append(str);
+                builder.Append(",");
+            }
+
+            builder.Remove(builder.Length - 1, 1);
+
+            return builder.ToString();
+        }
+
+        public static IList<string> StringToList(string longStr)
+        {
+            List<string> list = new List<string>();
+
+            if (string.IsNullOrEmpty(longStr))
+            {
+                return list;
+            }
+
+            string[] results = longStr.Split(CommonSeparator);
+
+            foreach (string str in results)
+            {
+                list.Add(str);
+            }
+
+            return list;
+        }
+
+        public static string DictionaryToString(IDictionary<string, string> subjectNodeSetIds)
+        {
+            if (subjectNodeSetIds == null || subjectNodeSetIds.Count == 0)
+            {
+                return null;
+            }
+
+            StringBuilder builder = new StringBuilder();
+
+            foreach (KeyValuePair<string, string> kv in subjectNodeSetIds)
+            {
+                builder.Append(kv.Key);
+                builder.Append(",");
+                builder.Append(kv.Value);
+                builder.Append(",");
+            }
+
+            builder.Remove(builder.Length - 1, 1);
+
+            return builder.ToString();
+        }
+
+        public static IDictionary<string, string> StringToDictionary(string jointSubjectNodeSetIds)
+        {
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+
+            if (string.IsNullOrEmpty(jointSubjectNodeSetIds))
+            {
+                return dict;
+            }
+
+            string[] result = jointSubjectNodeSetIds.Split(CommonSeparator);
+
+            if (result.Length % 2 != 0)
+            {
+                return dict;
+            }
+
+            for (int i = 0; i < result.Length; i+=2)
+            {
+                dict.Add(result[i], result[i + 1]);
+            }
+
+            return dict;
+        }
+        #endregion
+
         #region Json
-        //TODO: Use ServiceStack.Text Instead of.
+
         public static string ToJson(object domain)
         {
             return JsonConvert.SerializeObject(domain);
@@ -604,27 +689,6 @@ namespace HB.Framework.Common
             }
 
             return JsonConvert.DeserializeObject(jsonString, type);
-        }
-
-        #endregion
-
-        #region Time
-
-        //TODO: 对系统中的DateTime, DateTimeOffset,DateTime.UtcNow做出梳理和清理
-
-        public static long CurrentTimestampSeconds()
-        {
-            return DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-        }
-
-        public static long ToTimestampSeconds(DateTimeOffset dt)
-        {
-            return dt.ToUnixTimeSeconds();
-        }
-
-        public static DateTimeOffset ToDateTimeOffset(long timestampSeconds)
-        {
-            return DateTimeOffset.FromUnixTimeSeconds(timestampSeconds);
         }
 
         #endregion
