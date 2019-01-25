a498c91d89185df319ae11a8ab8a05ceebc9a838
yzbai <yzbai@brlite.com>
2019-01-25 17:10:03 +0800

    auto_commit

---
+++ test/HB.Framework.Database.Test/ComplexFieldTest.cs
@@ -0,0 +1,94 @@
+using HB.Framework.Database.SQL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace HB.Framework.Database.Test
+{
+    public class ComplexFieldTest : IClassFixture<ServiceFixture>
+    {
+        private IDatabase database;
+        private ISQLBuilder sqlBuilder;
+        private ITestOutputHelper output;
+
+        public ComplexFieldTest(ITestOutputHelper testOutputHelper, ServiceFixture serviceFixture)
+        {
+            output = testOutputHelper;
+            database = serviceFixture.Database;
+            sqlBuilder = serviceFixture.SQLBuilder;
+        }
+
+        [Fact]
+        public void Test_Create_Sql_For_TestEntity()
+        {
+            string sql = sqlBuilder.GetCreateStatement(typeof(TestEntity), false);
+
+            output.WriteLine(sql);
+        }
+
+        [Fact]
+        public void Test_Select_Sql_For_TestEntity()
+        {
+            IDbCommand command = sqlBuilder.CreateRetrieveCommand<TestEntity>();
+
+            output.WriteLine(command.CommandText);
+
+        }
+
+        [Fact]
+        public void Test_Retrieve_TestEntity()
+        {
+            IList<TestEntity> testEntities = database.RetrieveAll<TestEntity>();
+
+            Assert.NotEmpty(testEntities);
+            Assert.NotEmpty(testEntities[0].Books);
+            Assert.NotEmpty(testEntities[0].BookAuthors);
+        }
+
+        [Fact]
+        public void Test_Add_TestEntity()
+        {
+            TestEntity entity = new TestEntity();
+            entity.Name = "EntityName";
+            entity.Books = new List<string>() { "Cat", "Dog" };
+            entity.BookAuthors = new Dictionary<string, string>() { { "Cat", "XMan" }, { "Dog", "YMan" } };
+
+            DatabaseResult result = database.Add<TestEntity>(entity);
+
+            Assert.True(result.IsSucceeded());
+        }
+
+        [Fact]
+        public void Test_Update_TestEntity()
+        {
+            IList<TestEntity> testEntities = database.RetrieveAll<TestEntity>();
+
+            Assert.NotEmpty(testEntities);
+
+            TestEntity entity = testEntities[0];
+
+            entity.Books.Add("New Book");
+            entity.BookAuthors.Add("New Book", "Yuzhaobai");
+
+            DatabaseResult result = database.Update(entity);
+
+            Assert.True(result.IsSucceeded());
+        }
+
+        [Fact]
+        public void Test_Delete_TestEntity()
+        {
+            IList<TestEntity> testEntities = database.RetrieveAll<TestEntity>();
+
+            Assert.NotEmpty(testEntities);
+
+            TestEntity entity = testEntities[0];
+
+            DatabaseResult result = database.Delete(entity);
+
+            Assert.True(result.IsSucceeded());
+        }
+    }
+}
