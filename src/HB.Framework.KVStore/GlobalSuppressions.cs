3fa836fe21f647bc221eb7eaa1aa160eb57d250d
yzbai <yzbai@brlite.com>
2019-02-19 04:14:39 +0800

    auto_commit

--- src/HB.Framework.KVStore/GlobalSuppressions.cs
+++ src/HB.Framework.KVStore/GlobalSuppressions.cs
@@ -5,4 +5,6 @@
 // a specific target and scoped to a namespace, type, member, etc.
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1717:Only FlagsAttribute enums should have plural names", Justification = "<Pending>", Scope = "type", Target = "~T:HB.Framework.KVStore.KVStoreResultStatus")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:HB.Framework.KVStore.Entity.KVStoreEntityDef.KeyPropertyInfos")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>", Scope = "member", Target = "~P:HB.Framework.KVStore.KVStoreOptions.KVStoreSchemas")]
 
