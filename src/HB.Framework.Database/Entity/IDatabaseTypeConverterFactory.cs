74d9a5625a43feb10bc225397f7739415a30c598
yzbai <yzbai@brlite.com>
2019-02-19 05:17:44 +0800

    auto_commit

---
+++ src/HB.Framework.Database/Entity/IDatabaseTypeConverterFactory.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace HB.Framework.Database.Entity
+{
+    public interface IDatabaseTypeConverterFactory
+    {
+        DatabaseTypeConverter GetTypeConverter(Type type);
+    }
+}
