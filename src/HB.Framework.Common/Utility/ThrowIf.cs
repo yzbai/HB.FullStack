b529aa72afcdc8022617528930182ec8b2e63f89
hulijuan <yzbai@brlite.com>
2019-05-29 08:02:25 +0800

    AUTO_PUSH_MAC

--- src/HB.Framework.Common/Utility/ThrowIf.cs
+++ src/HB.Framework.Common/Utility/ThrowIf.cs
@@ -6,6 +6,7 @@ namespace System
 {
     public static class ThrowIf
     {
+
         public static T ThrowIfNull<T>(this T o, string paramName) where T : class
         {
             if (o == null)
