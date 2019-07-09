5013d6f38c8790f88c88181c98d721d98102e3e8
yzbai <yzbai@brlite.com>
2019-07-09 17:50:57 +0800

    dd

--- src/HB.Infrastructure.SQLite/SQLiteExecuterAsync.cs
+++ src/HB.Infrastructure.SQLite/SQLiteExecuterAsync.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Data;
 using Microsoft.Data.Sqlite;
-using Microsoft.Data.Sqlite;
 
 namespace HB.Infrastructure.SQLite
 {
