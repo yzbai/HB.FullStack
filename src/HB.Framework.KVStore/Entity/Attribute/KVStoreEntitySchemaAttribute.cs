ef0484b6752596a5569ae47b292673f803b814f3
yzbai <yzbai@brlite.com>
2019-07-20 17:25:47 +0800

    auto_commit

---
+++ src/HB.Framework.KVStore/Entity/Attribute/KVStoreEntitySchemaAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HB.Framework.KVStore.Entity
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class KVStoreEntitySchemaAttribute : Attribute
+    {
+
+    }
+}
