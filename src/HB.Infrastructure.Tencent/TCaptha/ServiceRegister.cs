58aba77fe53901d1b5b20accb04499037e000a7b
yzbai <yzbai@brlite.com>
2019-07-24 18:06:54 +0800

    auto_commit

---
+++ src/HB.Infrastructure.Tencent/TCaptha/ServiceRegister.cs
@@ -0,0 +1,52 @@
+﻿using HB.Infrastructure.Tencent;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceRegister
+    {
+        public static IServiceCollection AddTCaptha(this IServiceCollection services, Action<TCapthaOptions> action)
+        {
+            ThrowIf.Null(action, nameof(action));
+
+            services.AddOptions();
+
+            services.Configure(action);
+
+            TCapthaOptions options = new TCapthaOptions();
+            action(options);
+
+            AddTCapthaCore(services, options);
+
+            return services;
+        }
+
+        public static IServiceCollection AddTCaptha(this IServiceCollection services, IConfiguration configuration)
+        {
+            ThrowIf.Null(configuration, nameof(configuration));
+
+            services.AddOptions();
+
+            services.Configure<TCapthaOptions>(configuration);
+
+            TCapthaOptions options = new TCapthaOptions();
+            configuration.Bind(options);
+
+            AddTCapthaCore(services, options);
+
+            return services;
+        }
+
+        private static void AddTCapthaCore(IServiceCollection services, TCapthaOptions options)
+        {
+            services.AddHttpClient(TCapthaOptions.EndpointName, httpClient => {
+                httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+                httpClient.DefaultRequestHeaders.Add("User-Agent", typeof(ITCapthaClient).FullName);
+            });
+
+            services.AddSingleton<ITCapthaClient, TCapthaClient>();
+        }
+
+    }
+}
---
+++ src/HB.Infrastructure.Tencent/TCaptha/TCapthaOptions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HB.Infrastructure.Tencent
+{
+    public class TCapthaOptions : IOptions<TCapthaOptions>
+    {
+        public const string EndpointName = "Tecent_Captha";
+
+        public TCapthaOptions Value => this;
+
+        public string Endpoint { get; set; }
+
+        public IList<ApiKeySetting> ApiKeySettings { get; private set; } = new List<ApiKeySetting>();
+    }
+
+    public class ApiKeySetting
+    {
+        public string AppId { get; set; }
+
+        public string AppSecretKey { get; set; }
+    }
+}
