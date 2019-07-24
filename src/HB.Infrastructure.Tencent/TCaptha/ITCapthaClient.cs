36aa0beade681d807a42a3803554aab40172ff23
yzbai <yzbai@brlite.com>
2019-07-24 18:06:54 +0800

    auto_commit

---
+++ src/HB.Infrastructure.Tencent/TCaptha/ITCapthaClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HB.Infrastructure.Tencent
+{
+    public interface ITCapthaClient
+    {
+        Task<bool> VerifyTicket(string appid, string ticket, string randstr, string userIp);
+    }
+}
