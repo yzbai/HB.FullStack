86e90f0dd0effc717f3e8d2ebf2b5474ad8c7c9d
yzbai <yzbai@brlite.com>
2019-07-27 20:01:42 +0800

    auto_commit

--- src/HB.Framework.Http.SDK/IResourceClient.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace HB.Framework.Http.SDK
-{
-    public interface IResourceClient
-    {
-        Task<Resource<T>> GetAsync<T>(ResourceRequest request) where T : ResourceResponse;
-    }
-}
