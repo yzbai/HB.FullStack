22548da01d66ded163259e87c814925c5ba2ab24
yzbai <yzbai@brlite.com>
2019-12-04 18:04:29 +0800

    xx

---
+++ src/HB.Framework.Mobile/MobileNames.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HB.Framework.Mobile
+{
+    public class MobileNames
+    {
+        public const string AccessToken = "AccessToken";
+        public const string RefreshToken = "RefreshToken";
+        public const string CurrentUserGuid = "CurrentUserGuid";
+
+        public const string DeviceId = "DeviceId";
+        public const string DeviceVersion = "DeviceVersion";
+        public const string DeviceType = "DeviceType";
+        public const string DeviceAddress = "DeviceAddress";
+    }
+}
