2a7cca0077e009f80b42bc2d2ab15b281adb424c
yzbai <yzbai@brlite.com>
2019-12-16 17:15:26 +0800

    ss

---
+++ src/HB.Framework.Http/Utils/EnvironmentUtil.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System
+{
+    public static class EnvironmentUtil
+    {
+        public static bool IsDevelopment()
+        {
+            string aspnetcore_environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            if (!aspnetcore_environment.IsNullOrEmpty())
+            {
+                return aspnetcore_environment.Equals("Development", GlobalSettings.ComparisonIgnoreCase);
+            }
+
+            return false;
+        }
+    }
+}
