1eb2505346fe6f5d99be8212e23895a15a462e0e
白玉昭 <yzbai@brlite.com>
2019-12-24 06:26:12 +0800

    dd

---
+++ src/HB.Framework.Http/Utils/ModelStateDictionaryExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace System
+{
+    public static class ModelStateDictionaryExtensions
+    {
+        public static IDictionary<string, IEnumerable<string>> GetErrors(this ModelStateDictionary dicts)
+        {
+            Dictionary<string, IEnumerable<string>> errorDict = new Dictionary<string, IEnumerable<string>>();
+
+            if (dicts.IsNullOrEmpty())
+            {
+                return errorDict;
+            }
+
+            foreach (KeyValuePair<string, ModelStateEntry> states in dicts)
+            {
+                if (states.Value.ValidationState == ModelValidationState.Invalid)
+                {
+                    errorDict.Add(states.Key, states.Value.Errors.Select(me => me.ErrorMessage));
+                }
+            }
+
+            return errorDict;
+        }
+    }
+}
