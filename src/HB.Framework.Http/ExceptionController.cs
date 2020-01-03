e4171d0e368329f9a028019eb27f3a5031a4e8ac
yzbai <yzbai@brlite.com>
2020-01-03 17:40:30 +0800

    hh

---
+++ src/HB.Framework.Http/ExceptionController.cs
@@ -0,0 +1,36 @@
+﻿using HB.Framework.Common.Api;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HB.Framework.Http
+{
+    [ApiController]
+    public class ExceptionController : ExtendedController
+    {
+        private ILogger _logger;
+
+        public ExceptionController(ILogger<ExceptionController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("exception")]
+        public IActionResult Exception()
+        {
+            IExceptionHandlerFeature exceptionHandlerFeature = HttpContext.Features.Get<IExceptionHandlerFeature>();
+
+            _logger.LogException(exceptionHandlerFeature.Error);
+
+            ApiErrorResponse apiErrorResponse = new ApiErrorResponse(ApiError.EXCEPTIONTHROWN, exceptionHandlerFeature.Error.Message);
+
+            return new BadRequestObjectResult(apiErrorResponse)
+            {
+                ContentTypes = { "application/problem+json" }
+            };
+        }
+    }
+}
