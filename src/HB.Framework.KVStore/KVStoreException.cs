bdc1db7731d7a158f9bba438fc4e83f41d6e29af
yzbai <yzbai@brlite.com>
2020-05-05 07:33:03 +0800

    EventBusException和KVStoreException都继承FrameworkException

--- src/HB.Framework.KVStore/KVStoreException.cs
+++ src/HB.Framework.KVStore/KVStoreException.cs
@@ -6,63 +6,20 @@ using System.Text;
 
 namespace HB.Framework.KVStore
 {
-    public class KVStoreException : Exception
+    public class KVStoreException : FrameworkException
     {
         private IDictionary? _data;
 
-        public KVStoreError Error { get; private set; }
-
-        public int InnerNumber { get; private set; }
-
-        public string? InnerState { get; private set; }
-
-        public string? EntityName { get; private set; }
-
-        public string? Operation { get; private set; }
-
-        public KVStoreException(KVStoreError error, string entityName, string? message, Exception? innerException = null, [CallerMemberName]string operation = "") : base(message, innerException)
+        public override IDictionary Data
         {
-            Error = error;
-            Operation = operation;
-            EntityName = entityName;
-
-        }
-
-        public KVStoreException(Exception innerException, string entityName, string message, [CallerMemberName]string operation = "") :base(message, innerException)
-        {
-            Operation = operation;
-            EntityName = entityName;
-
-            if (innerException is KVStoreException kVStoreException)
+            get
             {
-                Error = kVStoreException.Error;
-                
-                InnerNumber = kVStoreException.InnerNumber;
-                InnerState = kVStoreException.InnerState;
-            }
-            else
-            {
-                Error = KVStoreError.UnKown;
-            }
-        }
-
-        public KVStoreException(int number, string state, string message, Exception? innerException = null) : base(message, innerException)
-        {
-            InnerNumber = number;
-            InnerState = state;
-            Error = KVStoreError.InnerError;
-        }
-
-        public override IDictionary Data {
-            get {
                 if (_data is null)
                 {
                     _data = base.Data;
                 }
 
                 _data["KVStoreError"] = Error.ToString();
-                _data["InnerNumber"] = InnerNumber;
-                _data["InnerState"] = InnerState;
                 _data["EntityName"] = EntityName;
                 _data["Operation"] = Operation;
 
@@ -70,15 +27,37 @@ namespace HB.Framework.KVStore
             }
         }
 
+        public override FrameworkExceptionType ExceptionType { get => FrameworkExceptionType.KVStore; }
+
+        public KVStoreError Error { get; private set; }
+
+        public string? EntityName { get; private set; }
+
+        public string? Operation { get; private set; }
+
+        public KVStoreException(KVStoreError error, string entityName, string? message, Exception? innerException = null, [CallerMemberName] string operation = "") : this(message, innerException)
+        {
+            Error = error;
+            Operation = operation;
+            EntityName = entityName;
+
+        }
+
+        public KVStoreException(Exception innerException, string entityName, string message, [CallerMemberName] string operation = "") : this(message, innerException)
+        {
+            Operation = operation;
+            EntityName = entityName;
+        }
+
         public KVStoreException()
         {
         }
 
-        public KVStoreException(string message) : base(message)
+        public KVStoreException(string? message) : base(message)
         {
         }
 
-        public KVStoreException(string message, Exception innerException) : base(message, innerException)
+        public KVStoreException(string? message, Exception? innerException) : base(message, innerException)
         {
         }
     }
