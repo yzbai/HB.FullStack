e949d230c9b67daf088c1bee497f430c18773a54
yzbai <yzbai@brlite.com>
2020-05-05 09:21:03 +0800

    ss

--- src/HB.Infrastructure.Aliyun/Oss/IAliyunOssService.cs
+++ src/HB.Infrastructure.Aliyun/Oss/IAliyunOssService.cs
@@ -4,14 +4,16 @@ namespace HB.Infrastructure.Aliyun.Oss
 {
     public interface IAliyunOssService
     {
-        /// <exception cref="AliyunOssException"></exception>
+        /// <exception cref="HB.Infrastructure.Aliyun.Oss.AliyunOssException"></exception>
         AliyunStsToken GetUserDirectoryToken(string bucket, string userGuid, bool isRead);
 
-        /// <exception cref="AliyunOssException"></exception>
+        /// <exception cref="HB.Infrastructure.Aliyun.Oss.AliyunOssException"></exception>
         AliyunStsToken GetDirectoryToken(string bucket, string directory, string roleSessionName, bool isRead);
 
+        /// <exception cref="HB.Infrastructure.Aliyun.Oss.AliyunOssException"></exception>
         string GetOssEndpoint(string bucket);
 
+        /// <exception cref="HB.Infrastructure.Aliyun.Oss.AliyunOssException"></exception>
         string GetRegionId(string bucket);
 
         string UserBucketName { get; }
