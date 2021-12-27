f1e6ddbfd96a02a9bc077c82891d84d6aa88b5e0
yzbai <yzbai@brlite.com>
2021-12-28 06:31:18 +0800

    add

---
+++ src/HB.FullStack.Common/Api/Requests/GetByIdRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HB.FullStack.Common.Api.Requests
+{
+    public class GetByIdRequest<T> : ApiRequest<T> where T : ApiResource2
+    {
+        [NoEmptyGuid]
+        public Guid Id { get; set; }
+
+        public GetByIdRequest() : base(HttpMethodName.Get, "ById") { }
+
+        public GetByIdRequest(Guid id) : this()
+        {
+            Id = id;
+        }
+
+        public override string ToDebugInfo()
+        {
+            return $"GetByIdRequest. {GetType().FullName}";
+        }
+    }
+}
--- src/HB.FullStack.Common/Api/Requests/GetByIdsRequest.cs
+++ src/HB.FullStack.Common/Api/Requests/GetByIdsRequest.cs
@@ -21,22 +21,4 @@ namespace HB.FullStack.Common.Api.Requests
             return $"GetByIdsRequest. {GetType().FullName}";
         }
     }
-
-    public class GetByIdRequest<T> : ApiRequest<T> where T : ApiResource2
-    {
-        [NoEmptyGuid]
-        public Guid Id { get; set; }
-
-        public GetByIdRequest() : base(HttpMethodName.Get, "ById") { }
-
-        public GetByIdRequest(Guid id) : this()
-        {
-            Id = id;
-        }
-
-        public override string ToDebugInfo()
-        {
-            return $"GetByIdRequest. {GetType().FullName}";
-        }
-    }
 }
