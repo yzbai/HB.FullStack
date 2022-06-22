41080b3e7742c83cfcaa9f73ec6708002a23bff3
yzbai <yzbai@brlite.com>
2022-06-22 21:59:16 +0800

    add

--- src/HB.FullStack.Common/Api/Requests/ApiRequestExtensions.cs
+++ src/HB.FullStack.Common/Api/Requests/ApiRequestExtensions.cs
@@ -14,19 +14,17 @@ namespace HB.FullStack.Common.Api
 
             //step2: 填充Content，具体数据
 
-            //具体传递的数据
-            //包括Get的参数也放到body中去
-            
-            //TTTTTTTTTTTTTTTTTTTTTTT
-            httpRequest.Content = new StringContent(SerializeUtil.ToJson(request), Encoding.UTF8, "application/json");
-
-            //httpRequest.Content
-            //httpRequest.Headers.Add("Accept", "application/json");
-            
-
             if (request is IUploadRequest fileUpdateRequest)
             {
                 httpRequest.Content = BuildMultipartContent(fileUpdateRequest);
+            }
+            else
+            {
+                //具体传递的数据
+                //包括Get的参数也放到body中去
+
+                //TODO: 是否应该在Get的情况下，不添加Content
+                httpRequest.Content = new StringContent(SerializeUtil.ToJson(request), Encoding.UTF8, "application/json");
             }
 
             return httpRequest;
