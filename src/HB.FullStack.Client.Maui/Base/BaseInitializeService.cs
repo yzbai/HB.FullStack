433243f3e6e3972f3a8d1b3204ca16a5c41ed12e
yzbai <yzbai@brlite.com>
2022-11-06 15:54:21 +0800

    add

--- src/HB.FullStack.Client.Maui/Base/BaseInitializeService.cs
+++ src/HB.FullStack.Client.Maui/Base/BaseInitializeService.cs
@@ -15,19 +15,44 @@ namespace HB.FullStack.Client.Maui.Base
 {
     public class BaseInitializeService : IMauiInitializeService
     {
-        private readonly IEnumerable<Migration> _migrations;
-
-        public BaseInitializeService(IEnumerable<Migration> migrations)
-        {
-            _migrations = migrations;
-        }
-
         public void Initialize(IServiceProvider services)
         {
-            #region Logging & Exceptions
+            InitLog(services);
 
-            Globals.Logger = services.GetRequiredService<ILogger<BaseInitializeService>>();
+            InitGlobalExceptions();
 
+            InitDatabase(services);
+
+            InitServices(services);
+        }
+
+        private static void InitServices(IServiceProvider services)
+        {
+            //These services shold Run Construction
+            _ = services.GetRequiredService<StatusManager>();
+            _ = services.GetRequiredService<IOfflineManager>();
+        }
+
+        private static void InitDatabase(IServiceProvider services)
+        {
+            IDatabase database = services.GetRequiredService<IDatabase>();
+
+            //Currents.AppendingTasks.Add(
+
+            JoinableTasks.JoinableTaskFactory.Run(() =>
+
+
+
+                database.InitializeAsync(_migrations).ContinueWith(task =>
+                {
+                    //TODO: 这里先跑一下数据库，因为，有时候，IdBarrier会莫名其妙的第一次读不到数据
+                }, TaskScheduler.Default));
+
+            //TODO: 使用ApiClient获取一些初始化参数，或者私密信息
+        }
+
+        private static void InitGlobalExceptions()
+        {
             TaskScheduler.UnobservedTaskException += (sender, e) =>
             {
                 //TODO: 上报
@@ -48,31 +73,11 @@ namespace HB.FullStack.Client.Maui.Base
 
                 Currents.ShowToast("抱歉，发生了错误");
             });
+        }
 
-            #endregion
-
-            #region Database
-
-            IDatabase database = services.GetRequiredService<IDatabase>();
-
-            //Currents.AppendingTasks.Add(
-
-            JoinableTasks.JoinableTaskFactory.Run(() =>
-                database.InitializeAsync(_migrations).ContinueWith(task =>
-                {
-                    //TODO: 这里先跑一下数据库，因为，有时候，IdBarrier会莫名其妙的第一次读不到数据
-                }, TaskScheduler.Default));
-
-            //TODO: 使用ApiClient获取一些初始化参数，或者私密信息
-
-            #endregion
-
-            #region Need Initialize
-
-            StatusManager statusManager = services.GetRequiredService<StatusManager>();
-            IOfflineManager offlineManager = services.GetRequiredService<IOfflineManager>();
-
-            #endregion
+        private static void InitLog(IServiceProvider services)
+        {
+            Globals.Logger = services.GetRequiredService<ILogger<BaseInitializeService>>();
         }
     }
 }
