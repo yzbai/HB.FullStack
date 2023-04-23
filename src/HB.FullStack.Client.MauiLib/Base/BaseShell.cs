031521e01114ddbc13a684c0252d8a04ca7d5dc3
yzbai <yzbai@brlite.com>
2023-04-23 09:13:34 +0800

    add

--- src/HB.FullStack.Client.MauiLib/Base/BaseShell.cs
+++ src/HB.FullStack.Client.MauiLib/Base/BaseShell.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using HB.FullStack.Client.MauiLib.Components;
 using HB.FullStack.Client.MauiLib.Controls;
 
 using Microsoft.Maui.Controls;
