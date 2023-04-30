e3c551ab725954d8abc074d8183c4db1ce5a82c3
yzbai <yzbai@brlite.com>
2023-04-30 10:33:30 +0800

    add

--- src/HB.FullStack.Client/Components/KVManager/IKVManager.cs
+++ src/HB.FullStack.Client/Components/KVManager/IKVManager.cs
@@ -3,6 +3,8 @@ using System.Threading.Tasks;
 
 namespace HB.FullStack.Client.Components.KVManager
 {
+    //Remark:
+    //Cache的KV，需要思考污染问题；KVStore需要考虑持久性，污染问题；而这里的KV使用在客户端，只要最简单的实现
     public interface IKVManager
     {
         Task<T?> GetAsync<T>(string key);
